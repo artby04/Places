244f0edc7ba325eef37b2484c4d4461f3148fa81
Artyom Yarmoluk <a.s.yarmoluk@gmail.com>
2021-07-01 18:27:08 +0300

    App Icon

--- Places/MainViewController.swift
+++ Places/MainViewController.swift
@@ -19,6 +19,9 @@ class MainViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+      
+
     }
 
     // MARK: - Table view data source
@@ -36,12 +39,20 @@ class MainViewController: UITableViewController {
 
         cell.textLabel?.text = restaurantNames[indexPath.row]
         cell.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
-
+        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
+        cell.imageView?.clipsToBounds = true
         // Configure the cell...
 
         return cell
         
     }
+    
+    //MARK: - Table view delegate
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
+    }
+    
     // MARK: - Navigation
 
     /*
