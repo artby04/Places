db02a45c93dd7acea30a837fff037759611787c2
Artyom Yarmoluk <a.s.yarmoluk@gmail.com>
2021-07-01 23:36:17 +0300

    Custom view table cell

---
+++ Places/CustomTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTableViewCell.swift
+//  Places
+//
+//  Created by Artyom on 1.07.21.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+}
