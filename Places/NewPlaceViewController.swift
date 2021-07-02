f2742ad312fd8ef3e85182df4a1193f01f8581d4
Artyom Yarmoluk <a.s.yarmoluk@gmail.com>
2021-07-02 17:17:27 +0300

    Image Picker Controller

--- Places/NewPlaceViewController.swift
+++ Places/NewPlaceViewController.swift
@@ -19,6 +19,24 @@
      override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
          
          if indexPath.row == 0 {
+            
+            let actionSheet = UIAlertController(title: nil,
+                                                message: nil,
+                                                preferredStyle: .actionSheet)
+            let camera = UIAlertAction(title: "Camera", style: .default) { _ in
+                self.chooseImagePicker(source: .camera)
+            }
+            let photo = UIAlertAction(title: "Photo", style: .default) { _ in
+                self.chooseImagePicker(source: .photoLibrary)
+            }
+            
+            let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+            
+            actionSheet.addAction(camera)
+            actionSheet.addAction(photo)
+            actionSheet.addAction(cancel)
+            
+            present(actionSheet, animated: true)
              
          } else {
              view.endEditing(true)
@@ -38,5 +56,19 @@
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
+    }
+ }
+
+ // MARK: Work With Image
+ 
+ extension NewPlaceViewController {
+    func chooseImagePicker(source: UIImagePickerController.SourceType) {
+        if UIImagePickerController.isSourceTypeAvailable(source) {
+            let imagePicker =  UIImagePickerController()
+            imagePicker.allowsEditing = true
+            imagePicker.sourceType = source
+            present(imagePicker, animated: true, completion: nil)
+            
+        }
     }
  }
